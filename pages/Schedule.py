2eb3e2ec237606ce9422a93474fa212a9c941804
bblagburn96 <mvbwc78qxp@privaterelay.appleid.com>
2026-05-11 21:26:35 -0500

    Create Schedule.py

---
+++ pages/Schedule.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.title("📅 2026 Schedule")
+st.info("**July 25, 2026** - 2-Man Scramble @ Guntersville State Park")
+st.info("**August 2026** - Memorial Invitational @ Point Mallard")
